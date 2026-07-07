c3df82964ed9c970d22e0cfe1a20d707d3d5b1b0
tanhn <2320294153@qq.com>
2026-07-07 22:20:48 +0800

    "这是第三次从idea中提交到本地仓库"

---
+++ TestGit01/src/test02.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: tanhaonan
+ * @Date: 2026/7/7 - 07 - 07 - 21:58
+ */
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello git");
+    }
+}
