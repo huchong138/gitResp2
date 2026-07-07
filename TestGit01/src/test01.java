cfad8fc82a8b9e43fe8750d5f6ec8cd9c3405b41
tanhn <2320294153@qq.com>
2026-07-07 21:59:31 +0800

    "这是第二次从idea中提交到本地仓库"

---
+++ TestGit01/src/test01.java
@@ -0,0 +1,9 @@
+/**
+ * @Auther: tanhaonan
+ * @Date: 2026/7/7 - 07 - 07 - 21:58
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
